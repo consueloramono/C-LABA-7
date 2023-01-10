146c8216dd7a8b3b06658c5d0ff17cfc301c1cf1
consueloramono <94902362+consueloramono@users.noreply.github.com>
2023-01-10 14:34:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace laba7
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> nums = new List<int>();
+            Console.WriteLine("Заповнiть лiст значеннями 1 та 0 (Iнше значення зупинить заповнення)");
+            string n = "0";
+            while(n == "0" || n == "1")
+            {
+                n = Console.ReadLine();
+                if (n == "0" || n == "1") nums.Add((int) Convert.ToInt32(n));
+            }
+            int count_zero = 0;
+            int count_one = 0;
+            foreach (int x in nums)
+            {
+                if (x == 0) count_zero++;
+                else count_one++;
+            }
+            Console.WriteLine($"Нулiв - {count_zero}, одиниць - {count_one}");
+            int[] A = nums.ToArray();
+            foreach (int x in A) Console.Write(x + " ");
+        }
+    }
+}
